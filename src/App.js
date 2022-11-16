273722cbba622166e5f2a73324c82856d27f8fc5
Akarsh gupta <98002276+akarsh-gupta007@users.noreply.github.com>
2022-11-16 22:30:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './App.css';
+import Emi from "./Loan/Emi";
+function App() {
+  return (
+    <div className="App">
+      <Emi />
+    </div>
+  );
+}
+
+export default App;
