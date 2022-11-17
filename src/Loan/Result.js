e450289df56459ecef14451fafa4c1b3c22f8239
Akarsh gupta <98002276+akarsh-gupta007@users.noreply.github.com>
2022-11-17 08:26:33 +0530

    Update Result.js

--- src/Loan/Result.js
+++ src/Loan/Result.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Style.css";
 const Result = (props) => {
-  var EmiDEtails = [];
+
+const [emidetail,setEmidetails]=useState([]);
   function showTable(amount, rate, month) {
     // console.log(amount, rate, month + " Akarsh");
     const interests = (amount * (rate * 0.01)) / month;
     const emipermonth = ((amount / month) + interests).toFixed(2);
+    var temparray = [];
     for (let i = 1; i <= month; i++) {
-      EmiDEtails.push(emipermonth);
+      temparray.push(emipermonth);
     }
-    console.log(EmiDEtails);
+    // console.log(temparray);
+    setEmidetails(temparray)
   }
-  console.log(EmiDEtails);
+  console.log(emidetail , "history");
   return (
     <>
-    
       <div className="container">
-        
         {props.list.map((value, index) => {
           return (
             <div className="box">
